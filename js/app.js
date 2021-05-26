0ef7fd19f14904cccdc11e1e26bde262c6b86e59
Tomivan <gracejunkie20@gmail.com>
2021-05-26 02:50:01 +0100

    added mobile responsiveness and navigation functions

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function goToTop() {
+    document.documentElement.scrollTop = 0;
+}
+function welcome() {
+    window.scrollTo(0, document.getElementById('welcome').offsetTop)
+}
+function about() {
+    window.scrollTo(0, document.getElementById('about').offsetTop)
+}
+function contactUs() {
+    window.scrollTo(0, document.getElementById('contact-us').offsetTop)
+}
