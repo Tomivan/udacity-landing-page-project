7c6844aeaac52f361f7dcdb13dc405d277cf7399
Tomivan <gracejunkie20@gmail.com>
2021-08-25 11:56:19 +0100

    project committed to save

--- js/app.js
+++ js/app.js
@@ -10,3 +10,16 @@ function about() {
 function contactUs() {
     window.scrollTo(0, document.getElementById('contact-us').offsetTop)
 }
+
+const navbar = document.getElementById("nav");
+
+const lis = navbar.getElementsByClassName('list');
+
+for (let i = 0; i < lis.length; i++) {
+    lis[i].addEventListener("click", function() {
+        console.log("clicked");
+        const current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", ""); 
+        this.className += " active";
+  });
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a udacity project. It is a Landing page with javascript navigation. The project is built with html, css and Javascript.
